64f10bd0b10736a34d25d3956f30be9cb117ad60
textobey <dogsan_e@naver.com>
2023-09-11 15:37:07 +0900

    [ADD] 프로젝트 생성

---
+++ CoreBluetoothDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreBluetoothDemo
+//
+//  Created by 이서준 on 2023/09/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
